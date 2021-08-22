89d93761fd2b33ace9875e91b03c36e4a3ce64c8
deeliciouscode <schmiderdavid@gmail.com>
2021-08-22 22:23:41 +0200

    update run script to also work when directories were not yet created

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ -d "build/intermediaries" ]; then
+    echo ""
+else
+    mkdir -p "build/intermediaries";
+    echo "/build/intermediaries was created"
+fi
 
 ghc --make -o ./build/Interpreter.bry -outputdir ./build/intermediaries Main.hs
 echo $1 | ./build/Interpreter.bry
