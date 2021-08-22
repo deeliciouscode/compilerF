be87bf872c7b8fa648f8778a8ed3f0cb7aca2e90
deeliciouscode <schmiderdavid@gmail.com>
2021-08-22 21:44:33 +0200

    added .bry files to gitignore

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ghc --make -o ./build/Interpreter -outputdir ./build/intermediaries Main.hs
-echo $1 | ./build/Interpreter
+ghc --make -o ./build/Interpreter.bry -outputdir ./build/intermediaries Main.hs
+echo $1 | ./build/Interpreter.bry
