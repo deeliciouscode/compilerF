4b34bbb67a25decd2357a6f0914ab960b57c684d
deeliciouscode <schmiderdavid@gmail.com>
2021-08-24 21:42:44 +0200

    init emulator

---
+++ src/Emulator.hs
@@ -0,0 +1,19 @@
+module Emulator where
+
+-- data Instructions =
+--     Pushfun String |
+--     Pushval Expr |
+--     Reset |
+--     Pushparam Int |
+--     Makeapp |
+--     Slide Expr |
+--     Return |
+--     Halt |
+--     Call |
+--     Unwind |
+--     Operator Expr |
+--     Alloc |
+--     SlideLet Expr |
+--     Update
+--     deriving (Show)
+
