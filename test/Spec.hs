192f14efb6964796313d819f1b5b3065f8aec0c9
deeliciouscode <schmiderdavid@gmail.com>
2021-09-05 15:56:35 +0200

    init test framework

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Test.Hspec
+import Test.Hspec.Runner
+  (runSpec, defaultConfig, evaluateSummary, configFormatter)
+import Test.Hspec.Formatters (progress)
+
+spec :: Spec
+spec = do
+  describe "main" $ do
+    it "is evaluated" $
+      "main = 0" `shouldBe` "main = 0"
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+hspecProgress :: Spec -> IO ()
+hspecProgress spec =
+  evaluateSummary
+    =<< runSpec spec (defaultConfig {configFormatter = Just progress})
+
+-- $> hspecProgress spec
